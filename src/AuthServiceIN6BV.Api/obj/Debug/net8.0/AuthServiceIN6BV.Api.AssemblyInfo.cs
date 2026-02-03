1551373abc35dc4361663d6bef8234edccad1ebc
erodriguez-2024250 <erodriguez-2024250@kinal.edu.gt>
2026-02-03 17:21:43 -0600

    add: clase 03 de febrero del 2026.

--- src/AuthServiceIN6BV.Api/obj/Debug/net8.0/AuthServiceIN6BV.Api.AssemblyInfo.cs
+++ src/AuthServiceIN6BV.Api/obj/Debug/net8.0/AuthServiceIN6BV.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceIN6BV.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852f4a0679868d6fde62aa7144f742e4562a3f24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebbca6d0715e4a2c9e6fe82cac778154721d1a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceIN6BV.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceIN6BV.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
